63d7c1355e436272c0a8f09260a625a19d445929
Jim Park <jpark@jim80.net>
2019-04-22 11:01:54 -0700

    wip

---
+++ go/space-age/space_age.go
@@ -0,0 +1,12 @@
+//define age on various planets in the solar system
+package space
+
+// type Planet struct {
+// 	name           string
+// 	orbital_period float64
+// }
+planet := map[string]int{"Earth": 31557600}
+
+func Age(seconds int, planet string) float64 {
+	return 1
+}
