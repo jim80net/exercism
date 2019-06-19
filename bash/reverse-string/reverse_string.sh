35ad1477e250fbed59d762d7e2bd6dfe02d6ae40
Jim Park <jpark@jim80.net>
2019-06-19 15:46:08 -0700

    bash native answer

--- bash/reverse-string/reverse_string.sh
+++ bash/reverse-string/reverse_string.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
 #perl -0777e "print scalar reverse \"$@\""
+#rev <<< "$@"
 
-rev <<< "$@"
+reverse() {
+  local original=$1
+  local reversed=""
+  local length=${#original}
+
+  for(( index=length-1; index>=0; index-- ))
+  do
+    reversed+=${original:index:1}
+  done
+
+  echo $reversed
+}
+
+reverse "$@"
