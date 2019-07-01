b402c6c3bf3bbc9cc0c85338c33130dce4cdd7ad
Jim Park <jpark@jim80.net>
2019-07-01 16:40:50 -0700

    [go/space-age] cleanup the function

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -9,20 +9,21 @@ func EarthYears(seconds float64) float64 {
 	return seconds / 31557600
 }
 
+var planetYearsInAnEarthYear = map[Planet]float64{
+	"Earth":   1,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
 //Age returns the number of planet years
 func Age(seconds float64, planet Planet) float64 {
-	planetsToEarthYears := map[Planet]float64{
-		"Earth":   1,
-		"Mercury": 0.2408467,
-		"Venus":   0.61519726,
-		"Mars":    1.8808158,
-		"Jupiter": 11.862615,
-		"Saturn":  29.447498,
-		"Uranus":  84.016846,
-		"Neptune": 164.79132,
-	}
 	earthYears := EarthYears(seconds)
-	planetYears := earthYears / planetsToEarthYears[planet]
+	planetYears := earthYears / planetYearsInAnEarthYear[planet]
 	// fmt.Printf(
 	// 	"%v seconds == %v Earth Years == %v %v years\n",
 	// 	seconds,
