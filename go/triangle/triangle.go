523371ac4d6bc10af807108707c473753a0b4e6e
Jim Park <jpark@jim80.net>
2019-07-03 00:04:18 -0700

    [go/triangle] more terse

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -8,108 +8,28 @@ type Kind string
 
 // Pick values for the following identifiers used by the test program.
 const (
-	NaT = "NaT" // not a triangle
-	Equ = "Equ" // equilateral
-	Iso = "Iso" // isosceles
-	Sca = "Sca" // scalene
+	NaT Kind = "Not A Triangle" // not a triangle
+	Equ      = "Equilateral"    // equilateral
+	Iso      = "Isoosceles"     // isosceles
+	Sca      = "Scalar"         // scalene
 )
-
-// NumberOfSidesEqual compares each side with the others, incrementing count if theres a match
-func NumberOfSidesEqual(a, b, c float64) int {
-	count := 0
-	if a == b || a == c {
-		count++
-	}
-	if b == a || b == c {
-		count++
-	}
-	if c == a || c == b {
-		count++
-	}
-	return count
-}
-
-// PositiveLengthTest ensures triangle has positive length sides
-func PositiveLengthTest(a, b, c float64) bool {
-	if a <= 0 {
-		return false
-	}
-	if b <= 0 {
-		return false
-	}
-	if c <= 0 {
-		return false
-	}
-	return true
-}
-
-// InequalityTest ensures sum of lengths of any two sides are >= third side
-func InequalityTest(a, b, c float64) bool {
-	if a+b < c {
-		return false
-	}
-	if a+c < b {
-		return false
-	}
-	if b+c < a {
-		return false
-	}
-	return true
-}
-
-//IsNaN ensures all three sides are numbers
-func IsNaN(a, b, c float64) bool {
-	if math.IsNaN(a) {
-		return false
-	}
-	if math.IsNaN(b) {
-		return false
-	}
-	if math.IsNaN(c) {
-		return false
-	}
-	return true
-}
-
-//IsInf ensures all three sides are numbers
-func IsInf(a, b, c float64) bool {
-	if math.IsInf(a, 0) {
-		return false
-	}
-	if math.IsInf(b, 0) {
-		return false
-	}
-	if math.IsInf(c, 0) {
-		return false
-	}
-	return true
-}
 
 // KindFromSides determines the type of triangle
 func KindFromSides(a, b, c float64) Kind {
-	var k Kind
-
-	if NumberOfSidesEqual(a, b, c) == 3 {
-		k = Equ
+	if math.IsNaN(a+b+c) || math.IsInf(a+b+c, 0) {
+		return NaT
 	}
-	if NumberOfSidesEqual(a, b, c) == 2 {
-		k = Iso
+	if a <= 0 || b <= 0 || c <= 0 {
+		return NaT
 	}
-	if NumberOfSidesEqual(a, b, c) == 0 {
-		k = Sca
+	if a+b < c || a+c < b || b+c < a {
+		return NaT
 	}
-	if !PositiveLengthTest(a, b, c) {
-		k = NaT
+	if a == b && a == c {
+		return Equ
 	}
-	if !InequalityTest(a, b, c) {
-		k = NaT
+	if a == b || a == c || b == c {
+		return Iso
 	}
-	if !IsNaN(a, b, c) {
-		k = NaT
-	}
-	if !IsInf(a, b, c) {
-		k = NaT
-	}
-
-	return k
+	return Sca
 }
