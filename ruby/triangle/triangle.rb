01e29a59f4ae3024d1a252676b66c6a1db8704bf
Jim Park <jpark@jim80.net>
2019-07-04 23:49:36 -0700

    [ruby/triangle] init commit

---
+++ ruby/triangle/triangle.rb
@@ -0,0 +1,29 @@
+class Triangle
+  def initialize(sides)
+    @a = sides[0]
+    @b = sides[1]
+    @c = sides[2]
+  end
+
+  def equilateral?
+    is_a_triangle && @a == @b && @b == @c
+  end
+
+  def isosceles?
+    is_a_triangle && ( @a == @b || @a == @c || @b == @c )
+  end
+
+  def scalene?
+    is_a_triangle && ( @a != @b && @a != @c && @b != @c )
+  end
+
+  def is_a_triangle
+    @a > 0 &&
+      @b > 0 &&
+      @c > 0 &&
+      @a + @b >= @c &&
+      @a + @c >= @b &&
+      @b + @c >= @a
+  end
+
+end
