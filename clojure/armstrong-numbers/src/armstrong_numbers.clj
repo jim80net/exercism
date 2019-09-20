998fede8eb4096af4ce051794df29dafb11d2ff5
Jim Park <jimp@scribd.com>
2019-09-20 00:11:17 -0700

    [clojure] add armstrong_numbers

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,5 +1,22 @@
 (ns armstrong-numbers)
 
-(defn armstrong? [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn armstrong? [num]
+  "An armstrong number is equal to the sum of each character raised to the number of characters"
+  (==  num (armstrong_calc num)))
+
+(defn armstrong_calc [num]
+  "Sum each character raised to the number of characters"
+  (reduce + (map #(exp % (count (seq_num num))) (seq_num num))))
+
+(defn exp [x n]
+  "Raise x n times."
+  (reduce * (repeat n x)))
+
+
+(defn seq_num [num]
+  "Turn an number into a seq of its characters"
+  (map #(- (int %) 48) (str num)))
+
+
+
+
