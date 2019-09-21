df673b72d39edea37d3f2b6521d8b30cf86f9cac
Jim Park <jimp@scribd.com>
2019-09-20 23:37:27 -0700

    docstring correctly

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,22 +1,24 @@
-(ns armstrong-numbers)
+(ns armstrong-numbers
+  "Tells you whether a number is an armstrong number."
+)
 
-(defn armstrong? [num]
-  "An armstrong number is equal to the sum of each character raised to the number of characters"
-  (==  num (armstrong_calc num)))
-
-(defn armstrong_calc [num]
-  "Sum each character raised to the number of characters"
-  (reduce + (map #(exp % (count (seq_num num))) (seq_num num))))
-
-(defn exp [x n]
+(defn exp
   "Raise x n times."
+  [x n]
   (reduce * (repeat n x)))
 
-
-(defn seq_num [num]
+(defn seq_num
   "Turn an number into a seq of its characters"
+  [num]
   (map #(- (int %) 48) (str num)))
 
 
+(defn armstrong_calc
+  "Sum each character raised to the number of characters."
+  [num]
+  (reduce + (map #(exp % (count (seq_num num))) (seq_num num))))
 
-
+(defn armstrong?
+  "An armstrong number is equal to the sum of each character raised to the number of characters."
+  [num]
+  (==  num (armstrong_calc num)))
